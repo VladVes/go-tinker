f83287a34b0e1b6ce16f5281e0de3a857dfbb106
VladVes <vccs@inbox.ru>
2026-03-13 15:06:07 +0700

    add profile route handler

--- webfibersrv/webfibersrv.go
+++ webfibersrv/webfibersrv.go
@@ -1,14 +1,30 @@
 package webfibersrv
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/sirupsen/logrus"
 )
+
+const profileUnknown = "unknown"
 
 func Run() {
 	app := fiber.New()
 	app.Get("/address", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, go! Don't gitve up! Figth!")
+	})
+
+	app.Get("/profile", func(c *fiber.Ctx) error {
+		profileId := c.Query("profile_id", profileUnknown)
+		if profileId == "" {
+			profileId = profileUnknown
+		}
+		if profileId == profileUnknown {
+			return c.Status(fiber.StatusUnprocessableEntity).SendString("porofile_id is required")
+		}
+
+		return c.SendString(fmt.Sprintf("user Profile ID is: %s", profileId))
 	})
 
 	logrus.Fatal(app.Listen(":" + HttpPort))
