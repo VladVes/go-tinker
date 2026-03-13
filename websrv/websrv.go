35d9bdfcb86013e3d82e3400f9cd360950112326
VladVes <vccs@inbox.ru>
2026-03-13 14:14:13 +0700

    update websrv

--- websrv/websrv.go
+++ websrv/websrv.go
@@ -2,6 +2,7 @@ package websrv
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/sirupsen/logrus"
 )
@@ -11,44 +12,47 @@ const (
 	about    = "This web server is written in Go!"
 )
 
-func handleGetRoot(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte(greeting))
-	if err != nil {
-		// Ошибка логируется функцией WithError
-		logrus.WithError(err).Error("get roor handler error")
-	}
-}
-
-func handeGetAbout(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte(about))
-	if err != nil {
-		// Ошибка логируется функцией WithError
-		logrus.WithError(err).Error("about handler error")
-	}
-}
-
-func handleGetCourses(w http.ResponseWriter, r *http.Request) {
-	page := r.URL.Query().Get("page")
-	var pageCourses string
-	switch page {
-	case "":
-		pageCourses = "Enter course number!"
-	case "1":
-		pageCourses = "How to write your first \"Hello world\" in Go..."
-	case "2":
-		pageCourses = "How does garbage colletor works?"
-	default:
-		pageCourses = "Under construction"
-	}
-	_, err := w.Write([]byte(pageCourses))
-	if err != nil {
-		// Ошибка логируется функцией WithError
-		logrus.WithError(err).Error("courses handler error")
-	}
-}
-
 func Run() {
+	var logger = logrus.New()
+	logger.SetOutput(os.Stdout)
 	port := "8080"
+
+	handleGetRoot := func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte(greeting))
+		if err != nil {
+			// Ошибка логируется функцией WithError
+			logrus.WithError(err).Error("get roor handler error")
+		}
+	}
+
+	handeGetAbout := func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte(about))
+		if err != nil {
+			// Ошибка логируется функцией WithError
+			logrus.WithError(err).Error("about handler error")
+		}
+	}
+
+	handleGetCourses := func(w http.ResponseWriter, r *http.Request) {
+		page := r.URL.Query().Get("page")
+		var pageCourses string
+		switch page {
+		case "":
+			pageCourses = "Enter course number!"
+		case "1":
+			pageCourses = "How to write your first \"Hello world\" in Go..."
+		case "2":
+			pageCourses = "How does garbage colletor works?"
+		default:
+			pageCourses = "Under construction"
+		}
+		_, err := w.Write([]byte(pageCourses))
+		if err != nil {
+			// Ошибка логируется функцией WithError
+			logrus.WithError(err).Error("courses handler error")
+		}
+	}
+
 	http.HandleFunc("/", handleGetRoot)
 	http.HandleFunc("/about", handeGetAbout)
 	http.HandleFunc("/courses", handleGetCourses)
