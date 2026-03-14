47fed95c8e36163cf4a32c5a37286995866ce80a
VladVes <vccs@inbox.ru>
2026-03-14 21:55:40 +0700

    add json serialize example

--- webfibersrv/webfibersrv.go
+++ webfibersrv/webfibersrv.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/VladVes/go-tinker/v2/data"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 )
 
@@ -42,7 +43,7 @@ func Run() {
 
 		return c.SendString(fmt.Sprintf("Post id: %s, Likes: %s\n", postId, strconv.FormatInt(likes, 10)))
 	})
-	
+
 	app.Post("/likes/:postId", func(c *fiber.Ctx) error {
 		postId := c.Params("postId")
 
@@ -57,10 +58,28 @@ func Run() {
 
 	})
 
-
-	// Пример десериализации данные передаваемых в теле запсроса 
+	// Пример десериализации данные передаваемых в теле запсроса
 	// и сериализации отправляемого ответа
+	app.Post("/logs", func(c *fiber.Ctx) error {
+		var request data.CreateLogEntryRequest
+		if err := c.BodyParser(&request); err != nil {
+			return fmt.Errorf("body parser: %w", err)
+		}
 
+		logEntry := data.LogEntry{
+			ID:        uuid.New().String(),
+			Message:   request.Message,
+			Level:     request.Level,
+			Timestamp: request.Timestamp,
+		}
+
+		// Упрощенное хранение в памяти приложения
+		data.Logs = append(data.Logs, logEntry)
+
+		return c.JSON(data.CreateLogEntryResponse{
+			ID: logEntry.ID,
+		})
+	})
 
 	logrus.Fatal(app.Listen(":" + HttpPort))
 }
