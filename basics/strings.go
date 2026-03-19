1837e87bff98a4a15bb21ba346037d769ca11886
VladVes <vccs@inbox.ru>
2026-03-19 10:31:20 +0700

    add basic code snippets

---
+++ basics/strings.go
@@ -0,0 +1,13 @@
+package basics
+
+import "fmt"
+
+//20. Как можно обратиться к символу в строке
+func PrintCharByIndexStrDemo () {
+	var str string = "Привет"
+	ch := str[0] // 
+	fmt.Println(ch) // выведет 10тичное предстваление первого байта строки
+	runes := []rune(str) // нужно создать срез рун
+	fmt.Println(runes[0]) // выведет код первого символа в utf8 
+	fmt.Printf("%c", runes[0]) // выведет символ 
+}
