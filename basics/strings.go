143f91eec74580c061742b170211751f4b56f3e7
VladVes <vccs@inbox.ru>
2026-03-21 17:55:29 +0700

    add stings builder example

--- basics/strings.go
+++ basics/strings.go
@@ -1,6 +1,9 @@
 package basics
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // 15. Каким способом удобней записать многострочную строку или строку со спец символами в переменную?
 var query = `
@@ -21,4 +24,24 @@ func PrintCharByIndexStrDemo() {
 	runes := []rune(str)       // нужно создать срез рун
 	fmt.Println(runes[0])      // выведет код первого символа в utf8
 	fmt.Printf("%c", runes[0]) // выведет символ
+}
+
+// 40. В чем может быть проблема если использовать много оперций конкатенации
+// (к примеру при сборке HTML, SQL или при генерации отчёта)?
+// Какой есть механизм для эффективного работа со строками (в чем его эффективность)?
+// Составить строку из 3х частей ("Hello", " ", "World" )
+// не используя конкатенацию + и без шаблонизации с помощью fmt и записать её в переменную result?
+var (
+	sp1 = "Hello"
+	sp2 = "Go!"
+)
+
+func StrBldr() {
+	var sb strings.Builder
+	sb.WriteString(sp1)
+	sb.WriteRune(' ')
+	sb.WriteString(sp2)
+	sb.WriteByte('!')
+
+	fmt.Println(sb.String())
 }
