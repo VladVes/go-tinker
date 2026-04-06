bf417a7c4da9a8138773839fecb2d04d017fb528
VladVes <vccs@inbox.ru>
2026-04-06 20:33:06 +0700

    add orm crud - create

--- internal/models/movie.go
+++ internal/models/movie.go
@@ -12,4 +12,16 @@ type Movie struct {
 	Genre        string
 	ReleasedAt   time.Time
 	Descrtiption string
+	// для теста db.Automigrate
+	AdditionalInfo string
+
+	// Тип данных в Go: float64 подходит для хранения чисел с дробной частью.
+	// Соответствует типу NUMERIC в PostgreSQL по семантике (хранение дробных чисел).
+	// Обеспечивает достаточную точность для формата NUMERIC(3,1) (диапазон от −99.9 до 99.9).
+	// Тег GORM: gorm:"type:numeric(3,1)"
+	// Директива type: явно задаёт тип столбца в БД.
+	// numeric(3,1) — прямое указание создать столбец типа NUMERIC с:
+	// precision = 3 — общее количество цифр (включая целую и дробную части);
+	// scale = 1 — количество цифр после запятой.
+	Rating float64 `gorm:"type:numeric(3,1)"`
 }
