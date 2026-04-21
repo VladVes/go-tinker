17bb76782489febbe8608c8b0f8a404c0f3f75d3
VladVes <vccs@inbox.ru>
2026-04-21 16:10:46 +0700

    add hooks example

--- internal/models/review.go
+++ internal/models/review.go
@@ -1,5 +1,7 @@
 package models
 
+import "gorm.io/gorm"
+
 type Review struct {
 	ID    uint
 	Text  string
@@ -7,4 +9,24 @@ type Review struct {
 	// ----
 	MovieID uint
 	Movie   Movie `gorm:"foreignKey:MovieID;references:ID"`
+}
+
+// пример хука
+// Рейтинг пересчитывается автоматически после создания отзыва.
+// При добавлении нескольких отзывов среднее значение обновляется корректно.
+
+func (r *Review) AfterCreate(tx *gorm.DB) error {
+	var movie Movie
+	if err := tx.First(&movie, r.MovieID).Error; err != nil {
+		return err
+	}
+	newCount := movie.ReviewCount + 1
+	newRating := (movie.Rating*float64(movie.ReviewCount) + float64(r.Score)/float64(newCount))
+
+	return tx.Model(&Movie{}).
+		Where("id = ?", r.MovieID).
+		Updates(map[string]any{
+			"rating":        newRating,
+			"reviews_count": newCount,
+		}).Error
 }
